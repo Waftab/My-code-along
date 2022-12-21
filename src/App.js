279a92e61ef0bf9eee457511b363d994803436c4
Warda Aftab <wardaaftab20@gmail.com>
2022-12-21 23:54:29 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ export default function App() {
       <h1 className="title">Code Nation Creamery</h1>
       <div className="menu">
         <h2>Menu Choices:</h2>
+       < IceCream flavor = "chocolate"/>
+              < IceCream flavor = "strawberry"/>
+       < IceCream flavor = "mango"/>
+
       </div>
     </div>
+    
   );
+}
+function IceCream (props){
+  return (<div>
+    <ul>
+      <li>{props.flavor}</li>
+      
+      </ul>
+  </div>);
 }
